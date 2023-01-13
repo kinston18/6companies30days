9f8901b0e0751ce09747df3705e93aff9b78aec9
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-01-13 20:42:37 +0530

    Update Running_Sum_of_1d_Array.cpp

--- Miscellaneous/Running_Sum_of_1d_Array.cpp
+++ Miscellaneous/Running_Sum_of_1d_Array.cpp
@@ -10,3 +10,12 @@ Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 */
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+            for (int i = 1; i < nums.size(); i++) {
+                nums[i]+=nums[i-1];
+            }
+            return nums;
+        }
+};
