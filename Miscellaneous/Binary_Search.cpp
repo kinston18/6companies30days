acffa32db63a9cde93adc8fb2f4da93334d628c5
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-01-14 23:47:34 +0530

    Update Binary_Search.cpp

--- Miscellaneous/Binary_Search.cpp
+++ Miscellaneous/Binary_Search.cpp
@@ -12,4 +12,29 @@ Output: 4
 Explanation: 9 exists in nums and its index is 4
 
  */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0, e=nums.size()-1;
+        
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            
+        }
+        
+        return -1;
+        
+        }
+};
  
